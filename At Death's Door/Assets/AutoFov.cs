f570a50d9b23b5cdfdc5e55a7b91b512afe84aa5
UnKError <anime.rap09@gmail.com>
2022-05-07 19:52:26 +0000

    AutoFov for all my m8 with 4:3 aspec ratio :C

---
+++ At Death's Door/Assets/AutoFov.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class AutoFov : MonoBehaviour
+{
+    public GameObject worldBounders;
+    private void Start()
+    {
+        float orthoSize = worldBounders.GetComponent<Collider2D>().bounds.size.x * Screen.height / Screen.width * 0.5f;
+        Camera.main.orthographicSize = orthoSize;
+    }
+}
