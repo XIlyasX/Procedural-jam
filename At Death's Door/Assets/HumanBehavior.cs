322465bac095de98e39df6199a7e5860baa26e4d
3boud0 <3boud.contact@gmail.com>
2022-05-07 12:14:12 +0000

    Human controlled by player when possessed
    Added player controlled human when he's possessed, jump ability when grounded. Added ground layer for all grounds.

--- At Death's Door/Assets/HumanBehavior.cs
+++ At Death's Door/Assets/HumanBehavior.cs
@@ -12,34 +12,86 @@ public class HumanBehavior : MonoBehaviour
     [SerializeField] float walkingSpeed = 2f;
     float currentSpeed;
 
+    [SerializeField] bool isPossessed;
+
+    // Demon control variables
+    [SerializeField] float speed;
+    [SerializeField] float jumpForce;
+    [SerializeField] LayerMask groundMask;
+    float horizontalInput;
+    bool isGrounded;
+
     private void Start()
     {
         myParentRigidBody = GetComponentInParent<Rigidbody2D>();
     }
     private void Update()
     {
-        //FLiping the player dependign on the run away speed
-        if(runAwaySpeed > 0 || walkingSpeed > 0)
+        if (!isPossessed)
         {
-            parentTransform.localScale = new Vector3(1, 1, 1);
-        } else if(runAwaySpeed < 0 || walkingSpeed < 0)
-        {
-            parentTransform.localScale = new Vector3(-1, 1, 1);
+            //FLiping the player dependign on the run away speed
+            if (runAwaySpeed > 0 || walkingSpeed > 0)
+            {
+                parentTransform.localScale = new Vector3(1, 1, 1);
+            }
+            else if (runAwaySpeed < 0 || walkingSpeed < 0)
+            {
+                parentTransform.localScale = new Vector3(-1, 1, 1);
+            }
         }
+        else
+        {
+            if (myParentRigidBody.velocity.x > 0.3f)
+            {
+                parentTransform.localScale = new Vector3(1, 1, 1);
+            }
+            else if (myParentRigidBody.velocity.x < -0.3f)
+            {
+                parentTransform.localScale = new Vector3(-1, 1, 1);
+            }
 
-        //Human movement behavior
-        if (isAHumanKilled)
-        {
-            myParentRigidBody.velocity = new Vector2(runAwaySpeed, myParentRigidBody.velocity.y);
-        } else
-        {
-            myParentRigidBody.velocity = new Vector2(walkingSpeed, myParentRigidBody.velocity.y);
+            horizontalInput = Input.GetAxisRaw("Horizontal");
+            if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+            {
+                Jump();
+            }
         }
+    }
 
+    private void FixedUpdate()
+    {
+        if (!isPossessed)
+        {
+            //Human movement behavior
+            if (isAHumanKilled)
+            {
+                myParentRigidBody.velocity = new Vector2(runAwaySpeed, myParentRigidBody.velocity.y);
+            }
+            else
+            {
+                myParentRigidBody.velocity = new Vector2(walkingSpeed, myParentRigidBody.velocity.y);
+            }
+        }
+        else
+        {
+            // Ground cheking
+            isGrounded = Physics2D.Raycast(transform.position, Vector2.down, 2f, groundMask);
+
+            // Demon controlled velocity
+            myParentRigidBody.velocity = new Vector2(horizontalInput * speed * Time.deltaTime, myParentRigidBody.velocity.y);
+
+        }
+    }
+
+    void Jump ()
+    {
+        myParentRigidBody.velocity = new Vector2(myParentRigidBody.velocity.x, jumpForce);
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
+        if (isPossessed) return;
+
         if (collision.gameObject.tag == "Ground")
         {
             runAwaySpeed = -runAwaySpeed;
@@ -49,6 +101,8 @@ public class HumanBehavior : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (isPossessed) return;
+
         if (collision.gameObject.tag == "Hazard")
         {
             runAwaySpeed = -runAwaySpeed;
